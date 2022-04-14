c8074f0ffa2649d6834f61a123c145508e709f7a
armahin <armahin@cs.washington.edu>
2022-04-13 17:34:47 -0700

    updated code

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+const process_1 = require("process");
 const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
 const main = async () => {
@@ -10,8 +11,7 @@ const main = async () => {
         type: "postgresql",
         debug: !constants_1.__prod__,
     });
-    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
-    await orm.em.persistAndFlush(post);
+    const post = orm.em.create(Post_1.Post, process_1.title, {});
 };
 main();
 //# sourceMappingURL=index.js.map
